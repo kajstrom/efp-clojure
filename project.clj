0cad585b44e91542eca90dd7a063aabc07af1726
Kaj Ström <kaj.strom@kapsi.fi>
2018-04-07 19:57:46 +0300

    Dependencies and configuration for connecting to PostreSQL

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"][org.clojure/math.numeric-tower "0.0.4"]])
+  :dependencies [
+                  [org.clojure/clojure "1.9.0"]
+                  [org.clojure/math.numeric-tower "0.0.4"]
+                  [org.clojure/java.jdbc "0.7.5"]
+                  [org.postgresql/postgresql "42.2.2"]])
---
+++ src/efp_clojure/utils/pgsql.clj
@@ -0,0 +1,9 @@
+(ns efp-clojure.utils.pgsql)
+
+(def pg-db {:dbtype "postgresql"
+            :dbname "cljpgsql"
+            :host "0.0.0.0"
+            :user "cljpgsql"
+            :password "cljpgsql"
+            :ssl false
+            })
