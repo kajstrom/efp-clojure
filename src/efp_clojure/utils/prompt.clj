637de9f5ad9c8c0aa14745ebe5595ccef4c71ed3
Kaj Ström <kaj.strom@kapsi.fi>
2018-04-09 20:04:28 +0300

    New utility function make-bool-prompt

--- src/efp_clojure/utils/prompt.clj
+++ src/efp_clojure/utils/prompt.clj
@@ -1,4 +1,5 @@
-(ns efp-clojure.utils.prompt)
+(ns efp-clojure.utils.prompt
+  (:require [clojure.string :as s]))
 
 (defn make-string-prompt
   [message]
@@ -7,11 +8,32 @@
     (read-line))
   )
 
+(def truthy '("yes" "y"))
+(def falsy '("no" "n"))
+
+(defn in?
+  [v coll]
+  (some #(= v %) coll))
+
+(defn make-bool-prompt
+  "Makes a prompt for boolean y/n yes/no prompts"
+  [message]
+  (fn prompt []
+    (println message)
+    (let [resp (s/lower-case (s/trim (read-line)))]
+      (if (in? resp truthy)
+        true
+        (if (in? resp falsy)
+          false
+          (do
+            (println (str "Invalid input. Please enter a valid input: " (s/join ", " truthy) ", " (s/join ", " falsy)))
+            (prompt)))))))
+
 (defn make-int-prompt
   [message]
   (fn prompt []
     (println message)
-    (try (Integer/parseInt (clojure.string/trim (read-line)))
+    (try (Integer/parseInt (s/trim (read-line)))
       (catch NumberFormatException e
         (println "Invalid input! Please enter a valid integer.")
         (prompt)))))
@@ -21,3 +43,8 @@
   (fn []
     (println message)
     (Double/parseDouble (read-line))))
+
+(defn make-output
+  [message]
+  (fn []
+    (println message)))
