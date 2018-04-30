aa4cba400d82f6ca1e7df8d7b0a9ad7e2db7caa9
Kaj Ström <kaj.strom@kapsi.fi>
2018-04-30 13:05:30 +0300

    E39: Sorting Records

---
+++ src/efp_clojure/E39/employee_record.clj
@@ -0,0 +1,9 @@
+(ns efp-clojure.E39.employee-record)
+
+(def employees (list
+                {:first-name "John" :last-name "Johnson" :position "Manager" :sep-date "2016-12-31"}
+                {:first-name "Tou" :last-name "Xiong" :position "Software Engineer" :sep-date "2016-10-05"}
+                {:first-name "Michaela" :last-name "Michaelson" :position "District Manager" :sep-date "2015-12-19"}
+                {:first-name "Jake" :last-name "Jacobson" :position "Programmer" :sep-date nil}
+                {:first-name "Jacquelyn" :last-name "Jackson" :position "DBA" :sep-date nil}
+                {:first-name "Sally" :last-name "Weber" :position "Web Developer" :sep-date "2015-12-18"}))
