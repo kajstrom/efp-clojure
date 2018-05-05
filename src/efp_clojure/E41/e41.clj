ce96275d6cc03e9d364d0bfad46fb164c5aff50a
Kaj Ström <kaj.strom@kapsi.fi>
2018-05-05 21:06:54 +0300

    Bugs in E41

--- src/efp_clojure/E41/e41.clj
+++ src/efp_clojure/E41/e41.clj
@@ -3,6 +3,6 @@
 
 (defn run
   []
-  (let [names (slurp (clojure.java.io/resource "E41_names.dat"))
-        header (str "Total of " (count names) "names\n-----------------")]
-    (spit "resources/E41_names.out" (s/join "\n" (sort (s/split-lines names))))))
+  (let [names (s/split-lines (slurp (clojure.java.io/resource "E41_names.dat")))
+        header (str "Total of " (count names) " names\n-----------------\n\n")]
+    (spit "resources/E41_names.out" (str header (s/join "\n" (sort names))))))
